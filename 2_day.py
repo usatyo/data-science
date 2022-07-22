0aad978fbab26dffacf648c9153b7348c4895aba
usatyo <yu0424.math8128@gmail.com>
2022-07-22 15:40:15 +0900

    7.8

--- 2_day.py
+++ 2_day.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def A(m, x):
+def calc_A(m, x):
     ret = np.zeros((m + 1, m + 1))
     for i in range(len(x)):
         geo = np.array([[x[i] ** j] for j in range(m + 1)])
@@ -9,7 +9,7 @@ def A(m, x):
     return ret
 
 
-def T(m, x, t):
+def calc_T(m, x, t):
     ret = np.zeros((m + 1, 1))
     for i in range(len(x)):
         geo = np.array([[x[i] ** j] for j in range(m + 1)]) * t[i]
@@ -17,4 +17,10 @@ def T(m, x, t):
     return ret
 
 
+def calc_w(m, x, t):
+    A = calc_A(m, x)
+    T = calc_T(m, x, t)
+    w = np.dot(np.linalg.inv(A), T)
+    return w
 
+print(calc_w(2, np.array([-3,0,1]), np.array([9,0,1])))
